f39b678eb6ba2e4ba396ecf8f9eeb17b28591459
Amanda Koh <amanda@naucera.net>
2019-09-11 16:50:41 +1000

    Extract tower and disk counts.

--- App.cs
+++ App.cs
@@ -7,12 +7,15 @@ namespace TowerOfHanoi
     {
         public static void Main(string[] args)
         {
-            var puzzle = new Puzzle(towerCount: 3, diskCount: 4);
+            const int TowerCount = 3;
+            const int DiskCount = 3;
+
+            var puzzle = new Puzzle(towerCount: TowerCount, diskCount: DiskCount);
             var printer = new PuzzlePrinter(Console.Out);
 
             printer.Print(puzzle);
 
-            puzzle.MoveDisks(count: 4, from: 0, to: 2, eachMove: () => {
+            puzzle.MoveDisks(count: DiskCount, from: 0, to: TowerCount - 1, eachMove: () => {
                 Console.Out.WriteLine();
                 printer.Print(puzzle);
             });
